ecc70a12a9f3cc3e449bca6a0a118d9e8533047e
Matheus-Bezerra <matheusbwzerra002@gmail.com>
2021-11-05 15:23:18 -0300

    Adicionando slides das tecnologias experimentadas

--- js/slides.js
+++ js/slides.js
@@ -8,7 +8,7 @@ const slides = [
         tecnologia: 'Css 3'
     },
     {
-        background: 'url(https://static.vecteezy.com/system/resources/thumbnails/001/236/806/small/yellow-gradient-background-with-stripe-lines-diagonal.jpg) no-repeat center/cover',
+        background: 'linear-gradient(to bottom, #FFAE01, #202020)',
         tecnologia: 'javascript'
     },
     {
@@ -16,7 +16,27 @@ const slides = [
         tecnologia: 'vue js'
     },
     {
-        background: '#202124 no-repeat center/cover',
+        background: 'url(https://www.xmple.com/wallpaper/red-black-linear-gradient-highlight-1920x1080-c2-000000-ff0000-l-33-a-165-f-21.svg) no-repeat center/cover',
         tecnologia: 'Java'
-    }
+    },
+    {
+        background: 'linear-gradient(to bottom, #3645a2, #DDD)',
+        tecnologia: 'linguagem C'
+    },
+    {
+        background: 'url(https://www.xmple.com/wallpaper/highlight-purple-linear-gradient-black-5120x2880-c2-000000-800080-l-67-a-345-f-21.svg) no-repeat center/cover',
+        tecnologia: 'php'
+    },
+    {
+        background: 'linear-gradient(to bottom, #005b82, #D88500)',
+        tecnologia: 'MYSQL'
+    },
+    {
+        background: 'linear-gradient(to bottom, #430134, #F25CEA)',
+        tecnologia: 'Adobe xd'
+    },
+    {
+        background: 'linear-gradient(to right, #D509C4, #5817A7, #4F26E7, #392DF1, #2330F4)',
+        tecnologia: 'figma'
+    },
 ]
