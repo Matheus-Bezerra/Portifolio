1321ea15a117c68bcf0bb1e55ed76dbd9d05451e
Matheus-Bezerra <matheusbwzerra002@gmail.com>
2021-11-05 13:55:56 -0300

    Adicionando primeiros sliders no array, simulando um arquivo Json

--- js/slides.js
+++ js/slides.js
@@ -1,5 +1,22 @@
 const slides = [
     {
-        
+        background: 'url(https://cdn.pixabay.com/photo/2016/01/12/13/10/image-1135418_1280.jpg) no-repeat center/cover',
+        tecnologia: 'Html 5'
+    },
+    {
+        background: 'url(https://media.istockphoto.com/photos/bright-blue-defocused-blurred-motion-abstract-background-picture-id1047234038?b=1&k=20&m=1047234038&s=170667a&w=0&h=LgmC445-NeWTNDMVV6q3kv2-djWejaG-yaNLczDEoAM=) no-repeat center/cover',
+        tecnologia: 'Css 3'
+    },
+    {
+        background: 'url(https://static.vecteezy.com/system/resources/thumbnails/001/236/806/small/yellow-gradient-background-with-stripe-lines-diagonal.jpg) no-repeat center/cover',
+        tecnologia: 'javascript'
+    },
+    {
+        background: 'url(https://wallpaperaccess.com/full/1155050.jpg) no-repeat center/cover',
+        tecnologia: 'vue js'
+    },
+    {
+        background: '#202124 no-repeat center/cover',
+        tecnologia: 'Java'
     }
 ]
