59c9652384b47ac2b9fe2e277835efb15559851b
Matheus-Bezerra <matheusbwzerra002@gmail.com>
2021-11-07 12:20:09 -0300

    separando os scripts

---
+++ js/projects.js
@@ -0,0 +1,58 @@
+// Seção dos Cards, inserindo via Javascript
+const projects = [
+    {
+        nome: 'deluxe Barber Shop',
+        imagem: './assets/img/port-project1.png',
+        link: 'https://matheus-bezerra.github.io/deluxeBarberShop/',
+        tipo: 'sites',
+    },
+    {
+        nome: 'Instagramer',
+        imagem: './assets/img/port-project2.png',
+        link: 'https://matheus-bezerra.github.io/Instagramer/',
+        tipo: 'landingPages',
+    },
+    {
+        nome: 'XboxX-S - Slider',
+        imagem: './assets/img/port-project3.png',
+        link: 'https://github.com/Matheus-Bezerra/XboxX-S',
+        tipo: 'landingPages',
+    },
+    {
+        nome: 'Cavaleiros do Zodíaco',
+        imagem: './assets/img/port-project4.png',
+        link: 'https://cocky-benz-1a912b.netlify.app/',
+        tipo: 'paginasInterativas',
+    },
+    {
+        nome: 'BookMark<i data-feather="bookmark"></i>',
+        imagem: './assets/img/port-project5.png',
+        link: 'https://github.com/Matheus-Bezerra/BookMark',
+        tipo: 'sites',
+    },
+    {
+        nome: 'batman<i data-feather="gitlab"></i>',
+        imagem: './assets/img/port-project6.png',
+        link: 'https://github.com/Matheus-Bezerra/Batman-Template',
+        tipo: 'landingPages',
+    },
+    {
+        nome: 'Fylo - dark',
+        imagem: './assets/img/port-project7.png',
+        link: 'https://github.com/Matheus-Bezerra/Fylo---Tema-escuro',
+        tipo: 'sites',
+    },
+    {
+        nome: 'Carros',
+        imagem: './assets/img/port-project8.png',
+        link: 'https://github.com/Matheus-Bezerra/ProjetoJs-carros',
+        tipo: 'paginasInterativas',
+    },
+    {
+        nome: 'Desafios Js',
+        imagem: './assets/img/port-project9.png',
+        link: 'https://github.com/Matheus-Bezerra/Desafios-Javascript',
+        tipo: 'paginasInterativas', 
+    }   
+]
+
