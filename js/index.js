0b5be69090a00adf6d3c040ceb17e909b0821896
Matheus-Bezerra <matheusbwzerra002@gmail.com>
2021-11-07 12:46:00 -0300

    Adicionando funcionalidade das seções do portifólio

--- js/index.js
+++ js/index.js
@@ -11,6 +11,7 @@ function optionClick() {
         const optionAttributes = option.getAttribute('data-option')
         if(selectedAttribute === optionAttributes) {
             option.classList.add('active')
+            createCards()
             return
         }
 
@@ -91,16 +92,57 @@ function changeTheme() {
 
 function createCards() {
     const sectionCard = document.querySelector('.portfolio-projetos')
+    sectionCard.innerHTML = ''
+    let html = ''
+    const options = document.querySelector('[data-option].active').getAttribute('data-option')
     projects.forEach((project, index) => {
-        sectionCard.innerHTML += `
-        <a href="${project.link}" target="_blank">
-            <div class="portfolio-projetosImg">
-                <img src="${project.imagem}" alt="projeto-${index}">
-                    <div class="main-img">${project.nome}</div>
-            </div>
-        </a>
-        `
+        switch (options) {
+            case 'todos':
+                html += `
+                <a href="${project.link}" target="_blank">
+                <div class="portfolio-projetosImg">
+                    <img src="${project.imagem}" alt="projeto-${index}">
+                        <div class="main-img">${project.nome}</div>
+                </div>
+                </a>
+                `
+            break;
+            case 'sites':
+                if(project.tipo == 'sites') {
+                    html += `<a href="${project.link}" target="_blank">
+                    <div class="portfolio-projetosImg">
+                        <img src="${project.imagem}" alt="projeto-${index}">
+                            <div class="main-img">${project.nome}</div>
+                    </div>
+                    </a>`
+                }
+            break;
+            case 'landingPages':
+                if(project.tipo == 'landingPages') {
+                    html += `<a href="${project.link}" target="_blank">
+                    <div class="portfolio-projetosImg">
+                        <img src="${project.imagem}" alt="projeto-${index}">
+                            <div class="main-img">${project.nome}</div>
+                    </div>
+                    </a>`
+                }
+            break;
+            case 'paginasInterativas':
+                if(project.tipo == 'paginasInterativas') {
+                    html += `<a href="${project.link}" target="_blank">
+                    <div class="portfolio-projetosImg">
+                        <img src="${project.imagem}" alt="projeto-${index}">
+                            <div class="main-img">${project.nome}</div>
+                    </div>
+                    </a>`
+                }
+            default:
+                break;
+        }
+        
     })
+    sectionCard.innerHTML += html
+    hoverCards()
     opcoes.forEach(option => {
         const projectCards = document.querySelectorAll('.portfolio-projetos a')
         const optionActive = option.classList.contains('active')
@@ -110,16 +152,19 @@ function createCards() {
 createCards()
 
 // Hover in cards
-const cards = document.querySelectorAll('.portfolio-projetos .portfolio-projetosImg')
-cards.forEach(card => {
-    card.addEventListener('mouseover', handleHoverCards)
-    card.addEventListener('mouseout', outHoverCards)
-})
-
-function handleHoverCards() {
-    this.classList.add('active')
-}
-
-function outHoverCards() {
-    this.classList.remove('active')
+function hoverCards() {
+    const cards = document.querySelectorAll('.portfolio-projetos .portfolio-projetosImg')
+    console.log(cards)
+    cards.forEach(card => {
+        card.addEventListener('mouseover', handleHoverCards)
+        card.addEventListener('mouseout', outHoverCards)
+    })
+    
+    function handleHoverCards() {
+        this.classList.add('active')
+    }
+    
+    function outHoverCards() {
+        this.classList.remove('active')
+    }
 }
