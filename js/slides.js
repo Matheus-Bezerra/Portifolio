c15ca3076ce939237d526aceae0d6b41c7bf91d0
Matheus-Bezerra <matheusbwzerra002@gmail.com>
2021-12-02 16:28:19 -0300

    js dos slides otimizado e estruturado

--- js/slides.js
+++ js/slides.js
@@ -1,42 +1,38 @@
 const slides = [
-    {
-        background: 'url(https://cdn.pixabay.com/photo/2016/01/12/13/10/image-1135418_1280.jpg) no-repeat center/cover',
-        tecnologia: 'Html 5'
-    },
-    {
-        background: 'url(https://media.istockphoto.com/photos/bright-blue-defocused-blurred-motion-abstract-background-picture-id1047234038?b=1&k=20&m=1047234038&s=170667a&w=0&h=LgmC445-NeWTNDMVV6q3kv2-djWejaG-yaNLczDEoAM=) no-repeat center/cover',
-        tecnologia: 'Css 3'
-    },
-    {
-        background: 'linear-gradient(to bottom, #FFAE01, #202020)',
-        tecnologia: 'javascript'
-    },
-    {
-        background: 'url(https://wallpaperaccess.com/full/1155050.jpg) no-repeat center/cover',
-        tecnologia: 'vue js'
-    },
-    {
-        background: 'url(https://www.xmple.com/wallpaper/red-black-linear-gradient-highlight-1920x1080-c2-000000-ff0000-l-33-a-165-f-21.svg) no-repeat center/cover',
-        tecnologia: 'Java'
-    },
-    {
-        background: 'linear-gradient(to bottom, #3645a2, #DDD)',
-        tecnologia: 'linguagem C'
-    },
-    {
-        background: 'url(https://www.xmple.com/wallpaper/highlight-purple-linear-gradient-black-5120x2880-c2-000000-800080-l-67-a-345-f-21.svg) no-repeat center/cover',
-        tecnologia: 'php'
-    },
-    {
-        background: 'linear-gradient(to bottom, #005b82, #D88500)',
-        tecnologia: 'MYSQL'
-    },
-    {
-        background: 'linear-gradient(to bottom, #430134, #F25CEA)',
-        tecnologia: 'Adobe xd'
-    },
-    {
-        background: 'linear-gradient(to right, #D509C4, #5817A7, #4F26E7, #392DF1, #2330F4)',
-        tecnologia: 'figma'
-    },
-]
+  {
+    image: './assets/img/HTML.png',
+    technology: 'Html 5',
+  },
+  {
+    image: './assets/img/CSS.png',
+    technology: 'Css 3',
+  },
+  {
+    image: './assets/img/Javascript.png',
+    technology: 'javascript',
+  },
+  {
+    image: './assets/img/React.png',
+    technology: 'React Js',
+  },
+  {
+    image: './assets/img/Java.png',
+    technology: 'Java',
+  },
+  {
+    image: './assets/img/Mysql.png',
+    technology: 'MySQL',
+  },
+  {
+    image: './assets/img/Php.png',
+    technology: 'Php',
+  },
+  {
+    image: './assets/img/lingC.png',
+    technology: 'Linguagem C',
+  },
+  {
+    image: './assets/img/Figma.png',
+    technology: 'Figma',
+  },
+];
--- js/sobre.js
+++ js/sobre.js
@@ -1,115 +1,92 @@
 // Menu Mobile!
-const menuList = document.querySelector('[data="menuList"]')
-const menuItem  = document.querySelector('header .app')
-menuItem.addEventListener('click', menuClick)
+const menuList = document.querySelector('[data="menuList"]');
+const menuItem = document.querySelector('header .app');
+menuItem.addEventListener('click', menuClick);
 
 function menuClick(event) {
-    const menuClass = menuList.classList.contains('active')
-    if(menuClass) {
-        menuList.classList.remove('active')
-        menuItem.innerHTML = '<img src="./assets/img/menu-white.svg" alt="menu-hamburguer">'
-    } else {
-        menuList.classList.add('active')
-        menuItem.innerHTML = `<img src="./assets/img/x-circle-white.svg" alt="menu-hamburguer">`
-    }
+  const menuClass = menuList.classList.contains('active');
+  if (menuClass) {
+    menuList.classList.remove('active');
+    menuItem.innerHTML =
+      '<img src="./assets/img/menu-white.svg" alt="menu-hamburguer">';
+  } else {
+    menuList.classList.add('active');
+    menuItem.innerHTML = `<img src="./assets/img/x-circle-white.svg" alt="menu-hamburguer">`;
+  }
 
-    modeIcon()
+  modeIcon();
 }
 
 // Função para mudar icone do menu MOBILE na alteração de tema e click do menu mobile
 function modeIcon() {
-    const header = document.querySelector('header').classList.contains('light')
-    const imgMenu = document.querySelector('header .app img')
-    const sourceMenu = imgMenu.getAttribute('src')
-    if(header) {
-        if(sourceMenu == './assets/img/x-circle-white.svg') {
-            imgMenu.setAttribute('src', './assets/img/x-circle.svg')
-        }
-
-        if(sourceMenu == './assets/img/menu-white.svg'){
-            imgMenu.setAttribute('src', './assets/img/menu.svg')
-        }
-    } else {
-        if(sourceMenu == './assets/img/x-circle.svg') {
-            imgMenu.setAttribute('src', './assets/img/x-circle-white.svg')
-        }
-        if(sourceMenu == './assets/img/menu.svg') {
-            imgMenu.setAttribute('src', './assets/img/menu-white.svg')
-        }
+  const header = document.querySelector('header').classList.contains('light');
+  const imgMenu = document.querySelector('header .app img');
+  const sourceMenu = imgMenu.getAttribute('src');
+  if (header) {
+    if (sourceMenu == './assets/img/x-circle-white.svg') {
+      imgMenu.setAttribute('src', './assets/img/x-circle.svg');
     }
+
+    if (sourceMenu == './assets/img/menu-white.svg') {
+      imgMenu.setAttribute('src', './assets/img/menu.svg');
+    }
+  } else {
+    if (sourceMenu == './assets/img/x-circle.svg') {
+      imgMenu.setAttribute('src', './assets/img/x-circle-white.svg');
+    }
+    if (sourceMenu == './assets/img/menu.svg') {
+      imgMenu.setAttribute('src', './assets/img/menu-white.svg');
+    }
+  }
 }
 
 // Modo noturno/claro
-const iconChangeTheme = document.querySelector('.change-theme')
-iconChangeTheme.addEventListener('click', changeTheme)
+const iconChangeTheme = document.querySelector('.change-theme');
+iconChangeTheme.addEventListener('click', changeTheme);
 
 function changeTheme() {
-    const body = document.querySelector('body').classList.toggle('light')
-    const header = document.querySelector('header').classList.toggle('light')
-    const footer = document.querySelector('footer').classList.toggle('light')
-    const previousSlide = document.querySelector('#previous').classList.toggle('light')
-    const nextSlide = document.querySelector('#next').classList.toggle('light')
-    console.log(previousSlide)
-    this.classList.toggle('light')
-    if(this.classList.contains('light')) {
-        iconChangeTheme.innerHTML = '<img src="./assets/img/moon.svg" alt="moon-icon">'
-        modeIcon()
-        return
-    }
-    iconChangeTheme.innerHTML = '<img src="./assets/img/sun.svg" alt="sun-icon">'
-    modeIcon()
+  const body = document.querySelector('body').classList.toggle('light');
+  const header = document.querySelector('header').classList.toggle('light');
+  const footer = document.querySelector('footer').classList.toggle('light');
+  const previousSlide = document
+    .querySelector('#previous')
+    .classList.toggle('light');
+  const nextSlide = document.querySelector('#next').classList.toggle('light');
+  console.log(previousSlide);
+  this.classList.toggle('light');
+  if (this.classList.contains('light')) {
+    iconChangeTheme.innerHTML =
+      '<img src="./assets/img/moon.svg" alt="moon-icon">';
+    modeIcon();
+    return;
+  }
+  iconChangeTheme.innerHTML = '<img src="./assets/img/sun.svg" alt="sun-icon">';
+  modeIcon();
 }
+// Slide
+const swiper = new Swiper('.mySwiper', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
 
 // slider adicionando informações automaticamente conforme o arquivo slide.js
-const sliderContainer = document.querySelector('.slider-container')
-slides.forEach(slide => {
-    sliderContainer.innerHTML += `
-    <div class="img" style="background: ${slide.background};">
-        <h1>${slide.tecnologia}</h1>
-    </div>`
-})
-
-// aplicando funcionalidade no slider
-const sliderEl = document.querySelector('.slider')
-const previousEl = sliderEl.querySelector('#previous')
-const nextEl = sliderEl.querySelector('#next')
-let interval = undefined
-let timeout = undefined
-
-
-const arrowsSlider = [previousEl, nextEl]
-arrowsSlider.forEach(arrow => arrow.addEventListener('click', handleClickSlider))
-
-function handleClickSlider() {
-    const sliderWidth = sliderContainer.offsetWidth
-    if(this.id == 'next') {
-        sliderContainer.scrollLeft += sliderWidth
-        stopScrollSlider()
-        return
-    }
-    sliderContainer.scrollLeft -= sliderWidth
-    stopScrollSlider()
-}
-
-function stopScrollSlider() {
-    clearTimeout(timeout)
-    clearInterval(interval)
-    interval = undefined
-    timeout = setTimeout(() => {
-        autoScrollSlider()
-    }, 5000)
-}
-
-function autoScrollSlider() {
-    interval = setInterval(() => {
-        const sliderWidth = sliderContainer.offsetWidth
-        const numberSlides = sliderContainer.childElementCount
-        const selectedSlide = (sliderContainer.scrollLeft/sliderWidth) + 1
-        if(numberSlides == Math.round(selectedSlide)) {
-            sliderContainer.scrollLeft = 0
-            return
-        }
-        sliderContainer.scrollLeft += sliderWidth
-    }, 2000)
-}
-autoScrollSlider()
+const sliderContainer = document.querySelector('.swiper-wrapper');
+slides.forEach(({ image, technology }) => {
+  sliderContainer.innerHTML += `
+  <div class="swiper-slide">
+    <img src=${image} alt=${technology}/>
+  </div>
+  `;
+});
